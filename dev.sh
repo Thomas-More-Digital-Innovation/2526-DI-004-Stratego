86e349b5a1a86d4e5dc797f182b28e08acf9b13b
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-03-30 19:28:04 +0200

    fix docker

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+docker compose up --build
--- update-db.sh
+++ update-db.sh
@@ -1,2 +1,2 @@
-docker compose -f docker-compose.dev.yml exec -T db psql -U stratego -d stratego < code/backend/schema.sql
+docker compose exec -T stratego psql -U stratego -d stratego < code/backend/schema.sql
 ./code/backend/seed.sh
