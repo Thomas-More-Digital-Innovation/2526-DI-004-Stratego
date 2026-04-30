d45fe13088f6032c453608008848bd96c3a3c71b
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-05-01 00:46:53 +0200

    fix fmt

--- code/backend/auth/const.go
+++ code/backend/auth/const.go
@@ -1,6 +1,6 @@
 package auth
 
 const (
-	MaxCookieAge        = 2 * 60 * 60           // 2 hours
-	MaxRefreshTokenAge = 30 * 24 * 60 * 60     // 30 days
+	MaxCookieAge       = 2 * 60 * 60       // 2 hours
+	MaxRefreshTokenAge = 30 * 24 * 60 * 60 // 30 days
 )
