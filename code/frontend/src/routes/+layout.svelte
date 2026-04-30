995f2a88722c8ac0e05048a9be540703da6ac79f
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-04-30 09:54:38 +0200

    add early access disclaimer

--- code/frontend/src/routes/+layout.svelte
+++ code/frontend/src/routes/+layout.svelte
@@ -110,4 +110,22 @@
             {@render children()}
         </div>
     </main>
+
+    <div
+        class="group fixed bottom-2 right-2 p-4 text-red-300 text-center bg-red-500/10 rounded-md"
+    >
+        <p class="text-sm">This game is early access, expect bugs.</p>
+        <div class="hidden group-hover:block">
+            <p class="text-xs">
+                Loss of data is possible. Thank you for testing!
+            </p>
+            <p class="text-sm">
+                Report any bugs <a
+                    class="underline text-brand-primary hover:text-brand-accent"
+                    href="https://github.com/Thomas-More-Digital-Innovation/2526-DI-004-Stratego/issues"
+                    >here</a
+                >.
+            </p>
+        </div>
+    </div>
 </div>
