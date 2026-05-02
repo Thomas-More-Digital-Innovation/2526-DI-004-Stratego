31b43095bef61450608c9a8d6ff549f88abe98d8
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-05-02 16:44:47 +0200

    show full disclaimer on home page

---
+++ code/frontend/src/lib/components/EarlyAccessDisclaimer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { page } from "$app/state";
+
+    let showFullDisclaimer = $derived(page.url.pathname === "/");
+</script>
+
+<div
+    class="group fixed bottom-2 right-2 p-4 text-red-300 text-center bg-red-500/10 rounded-md"
+>
+    <p class="text-sm">This game is early access, expect bugs.</p>
+    <div class={showFullDisclaimer ? "block" : "hidden group-hover:block"}>
+        <p class="text-xs">Loss of data is possible. Thank you for testing!</p>
+        <p class="text-sm">
+            Report any bugs <a
+                class="underline text-brand-primary hover:text-brand-accent"
+                href="https://github.com/Thomas-More-Digital-Innovation/2526-DI-004-Stratego/issues"
+                >here</a
+            >.
+        </p>
+    </div>
+</div>
--- code/frontend/src/routes/+layout.svelte
+++ code/frontend/src/routes/+layout.svelte
@@ -8,6 +8,7 @@
     import baseBg from "$lib/assets/background.webp";
     import profileBg from "$lib/assets/background-profile.webp";
     import boardBg from "$lib/assets/background-board-setup.webp";
+    import EarlyAccessDisclaimer from "$lib/components/EarlyAccessDisclaimer.svelte";
 
     let { children } = $props();
 
@@ -111,21 +112,5 @@
         </div>
     </main>
 
-    <div
-        class="group fixed bottom-2 right-2 p-4 text-red-300 text-center bg-red-500/10 rounded-md"
-    >
-        <p class="text-sm">This game is early access, expect bugs.</p>
-        <div class="hidden group-hover:block">
-            <p class="text-xs">
-                Loss of data is possible. Thank you for testing!
-            </p>
-            <p class="text-sm">
-                Report any bugs <a
-                    class="underline text-brand-primary hover:text-brand-accent"
-                    href="https://github.com/Thomas-More-Digital-Innovation/2526-DI-004-Stratego/issues"
-                    >here</a
-                >.
-            </p>
-        </div>
-    </div>
+    <EarlyAccessDisclaimer />
 </div>
