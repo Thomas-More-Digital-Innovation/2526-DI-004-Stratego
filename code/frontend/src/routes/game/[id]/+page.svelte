de90b616d4d71b5bf41ff603944dd646bce7d450
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-05-04 12:59:13 +0200

    add highlighting for active turn

--- code/frontend/src/routes/game/[id]/+page.svelte
+++ code/frontend/src/routes/game/[id]/+page.svelte
@@ -315,14 +315,16 @@
                         : setupSelectedPlayer === 0
                           ? [6, 7, 8, 9]
                           : [0, 1, 2, 3]
-                    : []}
+                    : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}
                 highlightColor={isSetupPhase
                     ? gameStore.gameMode.mode === gamemodes.human_vs_ai.mode
                         ? "red"
                         : setupSelectedPlayer === 0
                           ? "red"
                           : "blue"
-                    : ""}
+                    : gameStore.gameState?.currentPlayerId === 0
+                      ? "red"
+                      : "blue"}
                 scale={1.3}
                 lastMove={gameStore.lastMove}
             />
