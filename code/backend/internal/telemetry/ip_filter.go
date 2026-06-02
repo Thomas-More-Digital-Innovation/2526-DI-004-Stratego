f98f7173ade3b14b4f61749087f141830ebd8d9b
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-06-03 00:32:42 +0200

    unformat file for testing

--- code/backend/internal/telemetry/ip_filter.go
+++ code/backend/internal/telemetry/ip_filter.go
@@ -24,8 +24,10 @@ func IPFilterMiddleware() gin.HandlerFunc {
 	if allowedIPsStr == "" {
 		logging.Error("No allowed IPs configured. No one can access telemetry", nil)
 		return func(c *gin.Context) {
-			c.AbortWithStatusJSON(http.StatusForbidden, ginUnauthorizedIPaddress)
+			c.AbortWithStatusJSON(http.StatusForbidden, ginUnauthorizedIPaddress)  
 		}
+
+		
 	}
 
 	allowedIPs := strings.Split(allowedIPsStr, ",")
